4469cf18fc25845aecb43c300194399b8f95c60b
Anja <anja.mitrovic286@gmail.com>
2026-01-26 19:49:24 +0100

    Malo neke izmene da imamo i slike za korisnike

--- creative-backend/database/migrations/2026_01_18_223919_update_users_table_remove_email_verified_add_role.php
+++ creative-backend/database/migrations/2026_01_18_223919_update_users_table_remove_email_verified_add_role.php
@@ -12,16 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-             // uklanjamo email_verified_at
+            // uklanjamo email_verified_at
             if (Schema::hasColumn('users', 'email_verified_at')) {
                 $table->dropColumn('email_verified_at');
             }
 
-            // dodajemo nova polja 
+            // dodajemo role
             if (!Schema::hasColumn('users', 'role')) {
                 $table->enum('role', ['specialist', 'manager', 'admin'])
                     ->default('specialist')
                     ->after('password');
+            }
+
+            // dodajemo profile_photo
+            if (!Schema::hasColumn('users', 'profile_photo')) {
+                $table->string('profile_photo')->nullable()->after('role');
             }
         });
     }
@@ -32,8 +37,14 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            // vraćamo email_verified_at
             if (!Schema::hasColumn('users', 'email_verified_at')) {
                 $table->timestamp('email_verified_at')->nullable()->after('email');
+            }
+
+            // brišemo profile_photo
+            if (Schema::hasColumn('users', 'profile_photo')) {
+                $table->dropColumn('profile_photo');
             }
 
             // brišemo role
--- creative-backend/database/seeders/UserSeeder.php
+++ creative-backend/database/seeders/UserSeeder.php
@@ -20,6 +20,7 @@ class UserSeeder extends Seeder
                 'name' => 'Admin',
                 'role' => 'admin',
                 'password' => Hash::make('admin'),
+                 'profile_photo' => 'https://cdn.prod.website-files.com/6600e1eab90de089c2d9c9cd/6697274132daf24911799669_6635609f1211ec0f632803d6_fatzFxYM_ad86_raw.jpeg',
             ]
         );
 
@@ -29,6 +30,7 @@ class UserSeeder extends Seeder
                 'name' => 'Iva',
                 'role' => 'manager',
                 'password' => Hash::make('creative'),
+                'profile_photo' => 'https://cdn.prod.website-files.com/6600e1eab90de089c2d9c9cd/662c0927dc614ac9adfac27b_661a4b0ac0cb1ebd35610aa9_Woman.webp',
             ]
         );
         User::updateOrCreate(
@@ -37,6 +39,7 @@ class UserSeeder extends Seeder
                 'name' => 'Marija',
                 'role' => 'specialist',
                 'password' => Hash::make('creative'),
+                'profile_photo' => 'https://cdn.pixabay.com/photo/2025/01/21/20/39/ai-generated-9350464_1280.png',
             ]
         );
 
@@ -46,6 +49,7 @@ class UserSeeder extends Seeder
                 'name' => 'Anja',
                 'role' => 'specialist',
                 'password' => Hash::make('creative'),
+                'profile_photo' => 'https://petapixel.com/assets/uploads/2023/09/375727520_10161373112792533_7376944103131811808_n-646x800.jpg',
             ]
         );
 
@@ -55,6 +59,7 @@ class UserSeeder extends Seeder
                 'name' => 'Nikola',
                 'role' => 'specialist',
                 'password' => Hash::make('creative'),
+                'profile_photo' => 'https://i.pinimg.com/474x/60/5b/9b/605b9b86a82dd0147ed8aa612381326f.jpg',
             ]
         );
 
@@ -64,6 +69,7 @@ class UserSeeder extends Seeder
                 'name' => 'Jelena',
                 'role' => 'specialist',
                 'password' => Hash::make('creative'),
+                'profile_photo' => 'https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/short/ai_person_generator/ai_person_generator/webp/thumb_image/001.webp',
             ]
         );
     }
